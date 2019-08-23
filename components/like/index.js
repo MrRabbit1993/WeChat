f91e93ffa79d12a886b93b7fd54822128f988858
兔子先森 <1993iweb@gmail.com>
2019-08-23 15:22:04 +0800

    底层关注组件开发完成

--- components/like/index.js
+++ components/like/index.js
@@ -1,16 +1,25 @@
 Component({
   properties: {
-
+    like: {
+      type: Boolean
+    },
+    count: {
+      type: Number
+    }
   },
   data: {
-    like: true,
     ckeckUrl: "images/like.png",
-    notCkeckUrl: "images/like@dis.png",
-    count: 9
+    notCkeckUrl: "images/like@dis.png"
   },
   methods: {
     _touch() {
-      console.log(123)
+      let like  = this.properties.like;
+      let count = this.properties.count;
+      count = like?--count:++count;
+      this.setData({
+        like:!like,
+        count
+      })
     }
   }
 })
