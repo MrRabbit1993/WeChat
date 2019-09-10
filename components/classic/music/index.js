43928706e47009c0a359d6d24869a1017833a6c3
兔子先森 <1993iweb@gmail.com>
2019-09-10 11:43:05 +0800

    样式调整

--- components/classic/music/index.js
+++ components/classic/music/index.js
@@ -2,11 +2,13 @@ import classicBeh from "./../behavior.js";
 Component({
   behaviors: [classicBeh],
   properties: {
-
+    src:{
+      type: String,
+    }
   },
   data: {
-    pauseSrc: "images/player@waitting.png",
-    playSrc: "images/player@playing.png"
+    pauseSrc: "images/player@pause.png",
+    playSrc: "images/player@play.png"
 
   },
   methods: {
